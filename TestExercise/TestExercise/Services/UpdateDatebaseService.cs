066358e90dab54bec4f7dc86ca36a0614d9b3fc5
Poroshenco <1bl@ukr.net>
2018-02-26 23:22:28 +0200

    Project added

---
+++ TestExercise/TestExercise/Services/UpdateDatebaseService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TestExercise.Context;
+
+namespace TestExercise.Services
+{
+    public class UpdateDatebaseService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public UpdateDatebaseService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<OrderContext>();
+                await context.Database.MigrateAsync();
+            }
+        }
+    }
+}
